39177e5941fba01a4ebae131808767438542aaeb
xiaohui lam <xiaohui.lam@icloud.com>
2020-10-07 11:39:51 +0800

    fixbug

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,8 @@
 export const environment = {
   SERVER_URL: `./`,
-  production: true,
-  useHash: true,
+  apiUrl: `https://kfapi.ssls.com.cn/`,
+  socketHost: `http://kfapi.ssls.com.cn/`,
+  production: false,
+  useHash: false,
   hmr: false,
 };
