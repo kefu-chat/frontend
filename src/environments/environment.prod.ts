09c4356c40ddb5574a0f1572a156e8921fa8240b
xiaohui lam <xiaohui.lam@icloud.com>
2020-10-09 12:28:30 +0800

    it's production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   SERVER_URL: `./`,
   apiUrl: `https://kfapi.ssls.com.cn/`,
   socketHost: `https://kfapi.ssls.com.cn/`,
-  production: false,
+  production: true,
   useHash: false,
   hmr: false,
 };
