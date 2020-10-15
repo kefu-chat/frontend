510ebe7498014949b48086d02ef817cec303abb6
xiaoreign <xiaoreign@163.com>
2020-10-15 23:02:59 +0800

    UPDATE

--- src/app/routes/pro/enterprise/settings/plan/plan.component.ts
+++ src/app/routes/pro/enterprise/settings/plan/plan.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./plan.component.less"],
 })
 export class ProEnterpriseSettingsPlanComponent implements OnInit {
+  count = 4;
+  array = new Array(this.count);
   constructor() {}
 
   ngOnInit(): void {}
