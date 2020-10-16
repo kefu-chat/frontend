ce013c10afbee7a41f4956ed27235f5bc38c0d47
xiaohui lam <xiaohui.lam@icloud.com>
2020-10-16 14:31:57 +0800

    enhancement

--- src/style-icons.ts
+++ src/style-icons.ts
@@ -14,6 +14,7 @@ import {
   ThunderboltOutline,
   CheckCircleTwoTone,
   CloseCircleTwoTone,
+  QuestionCircleTwoTone,
 } from "@ant-design/icons-angular/icons";
 
 export const ICONS = [
@@ -30,4 +31,5 @@ export const ICONS = [
   PhoneOutline,
   CheckCircleTwoTone,
   CloseCircleTwoTone,
+  QuestionCircleTwoTone,
 ];
