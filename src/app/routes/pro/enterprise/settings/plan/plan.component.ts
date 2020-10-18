e92b455b7f07b454aba5b4eaca6fb95e900bd7e3
xiaohui.lam <xiaohui.lam@icloud.com>
2020-10-18 16:26:00 +0800

    升级订单管理

--- src/app/routes/pro/enterprise/settings/plan/plan.component.ts
+++ src/app/routes/pro/enterprise/settings/plan/plan.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from "@angular/core";
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { _HttpClient } from '@delon/theme';
-import { NzMessageService } from 'ng-zorro-antd/message';
+import { FormBuilder, FormGroup } from "@angular/forms";
+import { _HttpClient } from "@delon/theme";
+import { NzMessageService } from "ng-zorro-antd/message";
 
 export interface Plan {
   id: string;
@@ -10,7 +10,7 @@ export interface Plan {
   price_annually: null | number;
   price_biennially: null | number;
   price_triennially: null | number;
-  available: boolean
+  available: boolean;
   concurrent: number;
   desc: string;
   desensitize: boolean;
@@ -27,6 +27,26 @@ export interface Plan {
   updated_at?: string;
 }
 
+export const STATUS_UNPAID = 0;
+export const STATUS_PAID = 1;
+export const STATUS_CANCELLED = -1;
+
+export interface Order {
+  id: string;
+  enterprise_id: string;
+  plan_id: string;
+  user_id: string;
+  period: "monthly" | "annually" | "biennially" | "triennially";
+  coupon_id: null | string;
+  price: number;
+  need_pay_price: number;
+  paid_price: null | number;
+  status: 0 | 1 | -1;
+  created_at?: string;
+  updated_at?: string;
+  plan: Plan;
+}
+
 @Component({
   selector: "app-enterprise-settings-plan",
   templateUrl: "./plan.component.html",
@@ -41,6 +61,13 @@ export class ProEnterpriseSettingsPlanComponent implements OnInit {
   selectedPlan: Plan | null = null;
   periodsList: string[] = ["monthly", "annually", "biennially", "triennially"];
   upgradeForm: FormGroup;
+  orders: Order[] = [];
+  ordersTotal: number;
+  per_page = 5;
+  ordersLoading: boolean;
+  STATUS_UNPAID = STATUS_UNPAID;
+  STATUS_PAID = STATUS_PAID;
+  STATUS_CANCELLED = STATUS_CANCELLED;
 
   constructor(
     private http: _HttpClient,
@@ -49,6 +76,40 @@ export class ProEnterpriseSettingsPlanComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
+    this.orderList(1);
+    this.loadPlan();
+  }
+
+  select(plan_id: string): void {
+    this.selectedPlan = this.plans.filter((plan) => plan.id === plan_id)[0];
+    this.upgradeForm = this.fb.group({
+      period: "annually",
+    });
+  }
+
+  unselect(): void {
+    this.selectedPlan = null;
+  }
+
+  upgrade(): void {
+    this.http
+      .post(
+        `api/enterprise/plan/upgrade/${this.selectedPlan.id}`,
+        this.upgradeForm.getRawValue()
+      )
+      .subscribe(
+        (res: {
+          success: boolean;
+          message?: string;
+          data: { order: Order };
+        }) => {
+          this.orders.unshift(res.data.order);
+          this.unselect();
+        }
+      );
+  }
+
+  loadPlan(): void {
     this.http
       .get(`api/enterprise/plan`)
       .subscribe(
@@ -69,22 +130,40 @@ export class ProEnterpriseSettingsPlanComponent implements OnInit {
       );
   }
 
-  select(plan_id: string): void {
-    this.selectedPlan = this.plans.filter(plan => plan.id === plan_id)[0];
-    this.upgradeForm = this.fb.group({
-      period: "annually",
-    });
-  }
-
-  unselect(): void {
-    this.selectedPlan = null;
-  }
-
-  upgrade(): void {
+  orderList(page: null | number): void {
+    this.ordersLoading = true;
     this.http
-      .post(`api/enterprise/plan/upgrade/${this.selectedPlan.id}`, this.upgradeForm.getRawValue())
+      .get(`api/enterprise/plan/upgrade/order/list`, {
+        per_page: this.per_page,
+        page,
+      })
       .subscribe(
-        (res: { success: boolean; message?: string; data: any }) => {}
+        (res: {
+          success: boolean;
+          message?: string;
+          data: { list: { data: Order[]; total: number } };
+        }) => {
+          this.orders = res.data.list.data;
+          this.ordersTotal = res.data.list.total;
+          this.ordersLoading = false;
+          console.log(this.ordersTotal);
+        }
+      );
+  }
+
+  cancel(order: Order): void {
+    this.http
+      .post(
+        `api/enterprise/plan/upgrade/${order.id}/cancel`
+      )
+      .subscribe(
+        (res: {
+          success: boolean;
+          message?: string;
+          data: { order: Order };
+        }) => {
+          this.orders.filter(o => o.id === order.id)[0].status = STATUS_CANCELLED;
+        }
       );
   }
 }
