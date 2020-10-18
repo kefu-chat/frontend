c64b18e0e14830a11f63ac5f0fb345d62378c86e
xiaohui.lam <xiaohui.lam@icloud.com>
2020-10-18 13:16:50 +0800

    下单的组件 done

--- src/app/routes/pro/enterprise/settings/plan/plan.component.ts
+++ src/app/routes/pro/enterprise/settings/plan/plan.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { _HttpClient } from '@delon/theme';
 import { NzMessageService } from 'ng-zorro-antd/message';
 
@@ -31,29 +32,59 @@ export interface Plan {
   templateUrl: "./plan.component.html",
   styleUrls: ["./plan.component.less"],
 })
-
 export class ProEnterpriseSettingsPlanComponent implements OnInit {
   count = 4;
   array = new Array(this.count);
   plan: Plan;
   plans_available: Plan[];
   plans: Plan[] = [];
+  selectedPlan: Plan | null = null;
+  periodsList: string[] = ["monthly", "annually", "biennially", "triennially"];
+  upgradeForm: FormGroup;
 
   constructor(
     private http: _HttpClient,
-    private msg: NzMessageService
-  ) { }
+    private msg: NzMessageService,
+    private fb: FormBuilder
+  ) {}
 
   ngOnInit(): void {
-    this.http.get(`api/enterprise/plan`).subscribe((res: { success: boolean, message?: string, data: { plan: Plan, plans_available: Plan[] } }) => {
-      if (!res.success) {
-        this.msg.error(res.message);
-        return;
-      }
+    this.http
+      .get(`api/enterprise/plan`)
+      .subscribe(
+        (res: {
+          success: boolean;
+          message?: string;
+          data: { plan: Plan; plans_available: Plan[] };
+        }) => {
+          if (!res.success) {
+            this.msg.error(res.message);
+            return;
+          }
 
-      this.plan = res.data.plan;
-      this.plans_available = res.data.plans_available;
-      this.plans = [res.data.plan].concat(res.data.plans_available);
+          this.plan = res.data.plan;
+          this.plans_available = res.data.plans_available;
+          this.plans = [res.data.plan].concat(res.data.plans_available);
+        }
+      );
+  }
+
+  select(plan_id: string): void {
+    this.selectedPlan = this.plans.filter(plan => plan.id === plan_id)[0];
+    this.upgradeForm = this.fb.group({
+      period: "annually",
     });
+  }
+
+  unselect(): void {
+    this.selectedPlan = null;
+  }
+
+  upgrade(): void {
+    this.http
+      .post(`api/enterprise/plan/upgrade/${this.selectedPlan.id}`, this.upgradeForm.getRawValue())
+      .subscribe(
+        (res: { success: boolean; message?: string; data: any }) => {}
+      );
   }
 }
