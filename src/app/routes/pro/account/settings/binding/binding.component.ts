4c77b54cd90e6e25a60c0940f58897ba6d84ce3a
xiaohui lam <xiaohui.lam@icloud.com>
2020-12-01 16:42:54 +0800

    fixbug

--- src/app/routes/pro/account/settings/binding/binding.component.ts
+++ src/app/routes/pro/account/settings/binding/binding.component.ts
@@ -1,4 +1,4 @@
-import { Component, TemplateRef } from "@angular/core";
+import { Component, OnInit, TemplateRef } from "@angular/core";
 import { _HttpClient } from "@delon/theme";
 import { NzMessageService } from "ng-zorro-antd/message";
 import { NzModalService } from 'ng-zorro-antd/modal';
@@ -8,15 +8,30 @@ import { QRModule } from '@delon/abc/qr';
   selector: "app-account-settings-binding",
   templateUrl: "./binding.component.html",
 })
-export class ProAccountSettingsBindingComponent {
+export class ProAccountSettingsBindingComponent implements OnInit {
   bindSrc: string;
   loading = false;
+  bindings = [];
 
   constructor(
     public msg: NzMessageService,
     private modal: NzModalService,
     private http: _HttpClient
   ) {}
+
+  ngOnInit(): void {
+    this.loading = true;
+    this.http.get(`api/security/binding`).subscribe((res: {success: boolean, message?: string, data?: {list: string[]}}) => {
+      if (!res.success) {
+        this.msg.error(res.message);
+        return;
+      }
+
+      this.bindings = res.data.list;      
+    }, () => {
+      this.loading = false;
+    });
+  }
 
   bind(type: `wechat` | `wecom` | `dingtalk`, qrCode: TemplateRef<{}>): void {
     this.loading = true;
@@ -36,4 +51,19 @@ export class ProAccountSettingsBindingComponent {
       this.loading = false;
     });
   }
+
+  unbind(type: `wechat` | `wecom` | `dingtalk`): void {
+    this.loading = true;
+    this.http.get(`api/security/binding/${type}/unbind`).subscribe((res: {success: boolean, message?: string, data?: {qr: string}}) => {
+      if (!res.success) {
+        this.msg.error(res.message);
+        return;
+      }
+
+      this.loading = false;
+    }, () => {
+      this.loading = false;
+    });
+  }
+
 }
